489165cf63bfcfe9189eabbaa1568188aeafa263
Rani Itani <itani.rani@gmail.com>
2014-08-19 14:40:01 -0400

    step 5

--- crm.rb
+++ crm.rb
@@ -14,9 +14,5 @@ get '/contacts/new' do
 end 
 
 get '/contacts' do
-	@contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end 
