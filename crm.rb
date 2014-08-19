6cd6c5e4befe2ee2b3f91ef84c227fbd2d8cbe44
Rani Itani <itani.rani@gmail.com>
2014-08-19 14:25:04 -0400

    step 4

--- crm.rb
+++ crm.rb
@@ -1,4 +1,6 @@
+require_relative 'contact'
 require 'sinatra'
+
 
 get '/' do 
 	@crm_app_name = "My CRM"
@@ -10,5 +12,9 @@ get '/contacts/new' do
 end 
 
 get '/contacts' do
+	@contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end 
