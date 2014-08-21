5b8ccf68a5336f11def5c65b411f61f7be680707
Rani Itani <itani.rani@gmail.com>
2014-08-21 16:28:39 -0400

    added edit

--- crm.rb
+++ crm.rb
@@ -20,13 +20,41 @@ get '/contacts/new' do
 	erb :new_contact
 end 
 
-get "/contacts/1000" do
-  @contact = @@rolodex.find(1000)
-  erb :show_contacts
-end
-
 post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	@@rolodex.add_contact(new_contact)
 	redirect to('/contacts')
 end 
+
+get "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contacts
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
